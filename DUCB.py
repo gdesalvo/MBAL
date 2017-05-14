390dfa239ff45ce7822de957f34405360bed8b4c
Giulia DeSalvo <desalvo@linax2.cims.nyu.edu>
2017-05-14 16:31:02 -0400

    discucb

--- DUCB.py
+++ DUCB.py
@@ -6,7 +6,7 @@ matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 matplotlib.rcParams.update({'font.size': 22})
-random.seed(342)
+random.seed(34223)
 
 
 
@@ -86,14 +86,14 @@ def experts_rewards(K,T,process_type):
         
 
 
-def weights_q(curr_time,process_type,arm,mean_time_change):
+def calc_weights_q(curr_time,process_type):
     if process_type ==1 or process_type==2 or process_type==4:
         #IID, rotting arms, drifting
         q=(1.0/curr_time)*np.ones(curr_time)
 
-    elif process_type==3:
-        #changing means DOUBLE CHECK THIS
-        q=(1.0/(curr_time-mean_time_change[arm][np.argmax(mean_time_change[arm]>curr_times)] +1))*np.ones(curr_time)
+#    elif process_type==3: #add input to function mean_time_change and arm
+#        #changing means DOUBLE CHECK THIS
+#        q=(1.0/(curr_time-mean_time_change[arm][np.argmax(mean_time_change[arm]>curr_times)] +1))*np.ones(curr_time)
 
     elif process_type==6:
      #general process
@@ -104,22 +104,20 @@ def weights_q(curr_time,process_type,arm,mean_time_change):
     return q
 
 
-def emp_avg(rewards,weights_q):
-     return sum(rewards[:]*weights_q[:])
+def calc_emp_avg(rewards,weight):
+     return sum(rewards[:]*weight[:])
      
 
-#def slack(weight_q,time,beta):
-#    return np.linalg.norm(weight_q)*math.sqrt(beta*math.log(time))
+def calc_slack(weight_q,beta,time):
+    return np.linalg.norm(weight_q)*math.sqrt(beta*math.log(time))
 
 def slack(num_pulled,beta,T):
     return math.sqrt(beta*math.log(T)/(2.0*num_pulled) )
 
 
-def ucb(K,T,process_type): 
+def ucb(K,T,process_type,exp_rewards): 
     beta=3.0 #beta of UCB confidence
 
-    exp_rewards=experts_rewards(K,T,4)
-    print exp_rewards
     # #initialization step
     reward_alg = 0
     emp_avg=[0]*K
@@ -146,6 +144,39 @@ def ucb(K,T,process_type):
     return reward_alg / float(T) 
 
 
+def disc_ucb(K,T,process_type,exp_rewards): 
+    beta=3.0 #beta of UCB confidence
+
+#    exp_rewards=experts_rewards(K,T,4)
+#    print exp_rewards
+    #initialization step
+    reward_alg = 0
+    emp_avg=[0]*K
+    for i in range(K):
+        emp_avg[i]=[exp_rewards[0][i]]
+        reward_alg+= exp_rewards[0][i]
+        expert_pulls = [1.0] * K #everyone is pulled once in intiliazation step
+
+    for t in range(K, T):
+        #find best arm
+        weights_q= [calc_weights_q(expert_pulls[i],process_type)    for i in range(K) ]
+        ucb_list = [calc_emp_avg(emp_avg[i],weights_q[i]) + calc_slack(weights_q[i],beta,t) for i in range(K)] #soinefficient
+        best_arm = ucb_list.index(max(ucb_list)) 
+
+        best_expert_reward=exp_rewards[expert_pulls[best_arm]][best_arm]
+        expert_pulls[best_arm] += 1 #update number of times arm is pulled
+
+        #update emp average of expert
+        emp_avg[best_arm].append(best_expert_reward)
+        print best_arm
+
+        #update regret
+        reward_alg += best_expert_reward
+
+    return reward_alg / float(T) 
+
+
+
 
 
 if __name__ == "__main__":
@@ -159,7 +190,10 @@ if __name__ == "__main__":
 #    allrewards=experts_rewards(K,T,process_type)
 #    print allrewards
 #    wq=weights_q(curr_time,process_type)
-    print ucb(K,T,process_type)
+    exp_rewards=experts_rewards(K,T,process_type)
+    print exp_rewards
+    print ucb(K,T,process_type,exp_rewards)
+    print disc_ucb(K,T,process_type,exp_rewards)
 
 
     
